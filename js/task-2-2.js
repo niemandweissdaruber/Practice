8a677709c8317ad99eb7f2dd2b5ff9137477db19
Valerie <63469153+niemandweissdaruber@users.noreply.github.com>
2020-07-31 09:28:37 +0300

    Задания модуля 2

---
+++ js/task-2-2.js
@@ -0,0 +1,17 @@
+const calculateEngravingPrice = (message = '', pricePerWord = 0) => {
+  let totalPrice = 0;
+
+  totalPrice = message.split(' ').length * pricePerWord;
+
+  return totalPrice;
+};
+
+// console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+
+// console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
+
+//console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+
+//console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
+
+//console.log(calculateEngravingPrice('Uno', 100)); // 100
